99d2edc0d4b9b39b069276a0b91ffd70d5671ce6
神楽坂雅詩 <cyscxchope@gmail.com>
2015-08-29 16:28:57 +0800

    连接用户登录数据库

---
+++ check_session_login.php
@@ -0,0 +1,28 @@
+<?php
+include 'emostore_admin_sqllink.inc';
+@mysql_connect($db_host,$db_user,$db_password)
+or die("数据库连接失败");
+@mysql_select_db($db_name)
+or die("选择数据库失败");
+//获取输入的信息
+$username = $_POST['username'];
+$passcode = $_POST['passcode'];
+//获取session的值
+$query = @mysql_query("select username,userflag from users where username = '$username' and passcode = '$passcode'")
+or die("SQL语句执行失败");
+//判断用户以及密码
+if($row = mysql_fetch_array($query))
+{
+    session_start();
+    //判断权限
+    if($row['userflag'] == 1 or $row['userflag'] == 0){
+        $_SESSION['username'] = $row['username'];
+        $_SESSION['userflag'] = $row['userflag'];
+        echo "<a href='welcome_session_login.php'>登录成功</a>";
+    }else{
+        echo "userflag不正确";
+    }
+}else{
+    echo "username或者usercode";
+}
+?>
