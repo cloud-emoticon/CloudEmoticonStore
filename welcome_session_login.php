8fa0754313e619b57e42cc3c65556a79253fc014
0wew0 <wang124yi@163.com>
2015-08-29 17:09:19 +0800

    添加注销链接
    添加注销链接

--- welcome_session_login.php
+++ welcome_session_login.php
@@ -2,7 +2,7 @@
 session_start();
 if (isset($_SESSION['username'])) {
 	if ($_SESSION['userflag'] == 1) {
-		echo "欢迎管理员".$_SESSION['username']."登录";
+		echo "欢迎管理员".$_SESSION['username']."登录"."<a href='destroy_session_login.php'>注销</a>";
 	}
 	if ($_SESSION['userflag'] == 0) {
 		echo "欢迎用户".$_SESSION['username']."登录";
